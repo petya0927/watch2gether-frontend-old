1e95df26d9cba40b80d5495d8112087c5824e3c4
petya0927 <unyipeti@gmail.com>
2023-02-15 17:27:51 +0100

    changed cors policies in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,8 @@ const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: ['X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'],
   }
 });
 
