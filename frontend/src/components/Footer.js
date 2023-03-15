2e0a80480cbf5705c083e795e722089f50b98023
petya0927 <unyipeti@gmail.com>
2023-03-15 11:27:28 +0100

    Commit by deploy.py - version: 1.0.22

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = () => {
       <p className='text-white'>
         <FontAwesomeIcon icon={faCodeBranch} className="mr-5"/>
         <span>
-          Version: 1.0.21
+          Version: 1.0.22
         </span>
       </p>
     </div>
